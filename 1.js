2c9ef1bde5ed298c60718544426d02350e215bac
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-12-22 12:11:54 -0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,46 @@
+img="";
+objects=[];
+status=true;
+function preload(){
+img=loadImage("dog_cat.jpg");
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innetHTML="Status: Detecting Objects";
+
+}
+function draw(){
+    image(img ,0 ,0 ,640,420);
+    if(status!=""){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        for(i=0;i<objects.length; i++){
+    
+            document.getElementById("status").innerHTML="Status: Objects Detected!";
+            document.getElementById("num_of_obj").innerHTML="Number Of Objects Detecte Are: "+objects.length;
+            fill(r,g,b);
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+    
+}
+
+}
+ function modelLoaded(){
+     console.log("MODEL LOADED");
+     status=true;
+     objectDetector.detect(img,gotResult);
+ }
+ function gotResult(error,results){
+     if(error){
+         console.log(error);
+     }
+console.log(results);
+objects=results;
+ }
---
+++ main.js
@@ -0,0 +1,15 @@
+function gomegome1(){
+    window.location="1.html";
+}
+function gomegome2(){
+    window.location="2.html";
+}
+function gomegome3(){
+    window.location="3.html";
+}
+function gomegome4(){
+    window.location="4.html";
+}
+function gomegome5(){
+    window.location="5.html";
+}
